6dcee3b5d34ff64f3db56c1d69bf1f4a3dee4e18
Svitlana Ripa <svitlanka.ripa@gmail.com>
2025-08-08 12:07:26 +0200

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@
 // }
 
 
+import { use } from 'react';
 import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
@@ -118,17 +119,25 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  // const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const cuurentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const cuurentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+  // const cuurentSquares = history[history.length - 1];
+
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    // setHistory([...history, nextSquares]);
+    setCurrentMove(nextHistory.length - 1);
+    // setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-
+    setCurrentMove(nextMove);
+    // setXIsNext(nextMove % 2 === 0)
   }
 
 
